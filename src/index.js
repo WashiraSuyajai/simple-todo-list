14df5d88c44a36a8f4cc5a343f3621bf323c3d77
WashiraSuyajai <washira.suyajai@gmail.com>
2020-06-16 11:11:00 +0700

    Config Redux to Firebase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import { Provider } from "react-redux";
+import { createStore, compose } from "redux";
+import { reactReduxFirebase } from "react-redux-firebase";
+import firebase from "firebase";
+
+import App from "./App";
+import firebaseConfig from "./configFirebase";
+import rootReducer from "./reducers";
+
+import * as serviceWorker from "./serviceWorker";
+
+firebase.initializeApp(firebaseConfig);
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, { userProfile: "users" })
+)(createStore);
+
+const initialState = {};
+const store = createStoreWithFirebase(rootReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux"
+import { firebaseConfig as firebase} from "firebase"
+
+const rootReducer = combineReducers({
+    firebase
+})
+
+export default rootReducer
