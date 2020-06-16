97b2f5858c09f5c6e21fbfe7ec52a8900dc7fede
WashiraSuyajai <washira.suyajai@gmail.com>
2020-06-16 10:46:10 +0700

    First commit

---
+++ src/configFirebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_Hay4KGkKhq_D_tsAXMQ9Pi44Bx3aGUk",
+    authDomain: "todo-list-9d3d3.firebaseapp.com",
+    databaseURL: "https://todo-list-9d3d3.firebaseio.com",
+    projectId: "todo-list-9d3d3",
+    storageBucket: "todo-list-9d3d3.appspot.com",
+    messagingSenderId: "902806374226",
+    appId: "1:902806374226:web:5b493aafd6b848041e9c8b",
+    measurementId: "G-Q841DF26R7"
+  };
+
+  export default firebaseConfig
